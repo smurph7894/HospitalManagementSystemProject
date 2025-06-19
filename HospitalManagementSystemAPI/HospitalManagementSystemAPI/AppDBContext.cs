209d82b05557e06f87b9eec743b898cde31e8b0d
Sarah Murphree <104289709+smurph7894@users.noreply.github.com>
2025-06-19 14:18:42 -0700

    merge conflict with main fixed

--- HospitalManagementSystemAPI/HospitalManagementSystemAPI/AppDBContext.cs
+++ HospitalManagementSystemAPI/HospitalManagementSystemAPI/AppDBContext.cs
@@ -5,7 +5,7 @@ namespace HospitalManagementSystemAPI
 {
     public class AppDbContext : DbContext
     {
-        internal readonly object InventoryItems;
+        //internal readonly object InventoryItems;
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Admission> Admissions { get; set; }
